debe98f2fe05dd35c3134b5cdee66924efb93aa2
simonjanss <simon.jansson1@hotmail.com>
2021-01-08 09:35:52 +0100

    created service for filestore

---
+++ pkg/filestore/filestore.go
@@ -0,0 +1,57 @@
+package filestore
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+// Service handles the methods
+// for the filestore
+type Service interface {
+	Upload(name string, content []byte) (*File, error)
+	Delete(filepath string) error
+}
+
+type svc struct {
+	path string
+}
+
+// New creates a new Service
+func New(path string) (Service, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, err
+	}
+	return svc{path}, nil
+}
+
+// File is the object
+// with file-information
+type File struct {
+	Name string
+	Path string
+	Size int
+}
+
+func (s svc) Upload(name string, content []byte) (*File, error) {
+	if len(name) == 0 {
+		return nil, errors.New("specify name for file to upload")
+	}
+
+	path := fmt.Sprintf("%s/%s", s.path, name)
+	file, err := os.Create(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	size, err := file.Write(content)
+	if err != nil {
+		os.Remove(path)
+		return nil, err
+	}
+
+	return &File{Name: name, Path: path, Size: size}, nil
+}
+
+func (svc) Delete(filepath string) error { return os.Remove(filepath) }
